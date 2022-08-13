d2f1388d48caa95493126c35c53b19cd860c8fc4
Rodriguez jose luis <rodriguezjoseluis04@gmail.com>
2022-08-13 18:33:58 -0300

    primer entrega

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+
+    let nombre = prompt("ingrese su nombre");
+    let numero = parseInt(prompt("ingrese un numero del 1 al 100"));
+
+
+    for( let i = 0 ; i < numero; i++){
+
+       console.log ("nombre de usuario:" , nombre );
+
+       if(numero >100){
+         break;
+         
+       }
+       
+    }
+    
+   
+
+
+
+
+
+
+
+
+
